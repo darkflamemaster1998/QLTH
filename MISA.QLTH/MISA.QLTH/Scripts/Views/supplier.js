9b2d40e2932f41d74f054d7de28b933b74bf9cb2
bqlinh <darkflamemaster2321998@gmail.com>
2019-05-06 12:54:42 +0700

    Làm tiếp dialog

---
+++ MISA.QLTH/MISA.QLTH/Scripts/Views/supplier.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+    //$('#txtBirthday').datepicker();
+    //$("#cbxGender,#cbxStopFollow").selectmenu();
+    //Khởi tạo một object nhà cung cấp
+    var supplierJS = new SupplierJS();
+})
+
+var context;
+
+class SupplierJS {
+    //Hàm khởi tạo
+    //Created by BQLINH (5/5/2019)
+    constructor() {
+        this.initEvents();
+        this.dlgResgisterReader = new Dialog('.dlgResgisterReader', 800, 280, this);
+        context = this;
+    }
+
+    // Xử lý sự kiện
+    // Created by BQLINH (5/5/2019)
+    initEvents() {
+        $('#btnAdd').click(this.btnAddClick);
+        $('#btnEdit').click(this.btnEditClick);
+        $('#btnDelete').click(this.btnDeleteClick);
+        $('#btnRefresh').click(this.btnRefreshClick);
+        $('#btnHelp').click(this.btnHelpClick);
+    }
+
+    // Xử lý khi click vào nút thêm
+    // Created by BQLINH (5/5/2019)
+    btnAddClick() {
+        alert("Bạn vừa click vào nút thêm!");
+    }
+
+    // Xử lý khi click vào nút sửa
+    // Created by BQLINH (5/5/2019)
+    btnEditClick() {
+        alert("Bạn vừa click vào nút sửa!");
+    }
+
+    // Xử lý khi click vào nút xóa
+    // Created by BQLINH (5/5/2019)
+    btnDeleteClick() {
+        alert("Bạn vừa click vào nút xóa!");
+    }
+
+    // Xử lý khi click vào nút nạp
+    // Created by BQLINH (5/5/2019)
+    btnRefreshClick() {
+        alert("Bạn vừa click vào nút nạp!");
+    }
+
+    // Xử lý khi click vào nút giúp
+    // Created by BQLINH (5/5/2019)
+    btnHelpClick() {
+        alert("Bạn vừa click vào nút giúp!");
+    }
+}
---
+++ MISA.QLTH/MISA.QLTH/Scripts/base/dialog.js
@@ -0,0 +1,72 @@
+﻿//Class dialog 
+//Created by BQLINH (5/5/2019)
+class Dialog {
+    //Hàm khởi tạo
+    //Tham số (element, width, height, buttons, scope) : (phần tử, độ rộng phần tử, độ cao phần tử, các buttons truyền vào, biến phạm vi) 
+    //Created by BQLINH (5/5/2019)
+    constructor(element, width, height, buttons, scope) {
+        //debugger;
+        if (!buttons) {
+            buttons = this.buildButtons(scope ? scope : this);
+        }
+        this.dialog = $(element).dialog({
+            autoOpen: true,
+            height: height,
+            width: width,
+            modal: true,
+            buttons: buttons,
+            close: function () {
+
+            }
+        });
+    }
+    //Hàm tạo buttons
+    //Created by BQLINH (5/5/2019)
+    buildButtons(scope) {
+        var buttons = buttons = [{
+            text: 'Cất',
+            icons: {
+                primary: "ui-icon-check"
+            },
+            class: "",
+            id: "btnSave",
+            click: eval(scope)['btnSaveClick']
+        }, {
+            text: 'Hủy',
+            icons: {
+                primary: "ui-icon-cancel"
+            },
+            click: function () {
+                $(this).dialog('close');
+            }
+        },
+        {
+            text: 'Giúp',
+            icons: {
+                primary: "ui-icon-cancel"
+            },
+            click: function () {
+                $(this).dialog('close');
+            }
+        }];
+        return buttons;
+    }
+
+    //Hàm xử lý khi bấm cất trên dialog
+    //Created by BQLINH (5/5/20)
+    save() {
+        alert('Lưu');
+    }
+
+    //Hàm xử lý khi bấm hủy trên dialog
+    //Created by BQLINH (5/5/2019)
+    close() {
+        alert('Hủy');
+    }
+
+    //Hàm xử lý khi bấm giúp trên dialog
+    //Created by BQLINH (5/5/20)
+    help() {
+        alert('Giúp');
+    }
+}
